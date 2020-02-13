0631cc7e377d514305f799e9148327f9a7cb2286
maxiu271299 <maksymilian.kierski@gmail.com>
2020-02-13 22:03:57 +0100

    Add item store handler

--- app.js
+++ app.js
@@ -1,6 +1,54 @@
 //IIFE module to all functionality
 var budgetController = (function() {
-    //some code
+    var Income = function(id, description, value) {
+        this.id = id;
+        this.description = description;
+        this.value = value;
+    };
+
+    var Expanse = function(id, description, value) {
+        this.id = id;
+        this.description = description;
+        this.value = value;
+    };
+
+    var data = {
+        allItems: {
+            inc: [],
+            exp: [],
+        },
+
+        totals: {
+            inc: 0,
+            exp: 0,
+        },
+    };
+    return {
+        addItem: function(type, desc, val) {
+            var newItem, ID;
+
+            if (data.totals[type] > 0) {
+                ID = data.totals[type][data.allItems[type].length - 1].id + 1;
+            } else {
+                ID = 0;
+            }
+
+            if (type === 'inc') {
+                newItem = new Income(ID, desc, val);
+            }
+
+            if (type === 'exp') {
+                newItem = new Expanse(ID, desc, val);
+            }
+
+            data.allItems[type].push(newItem);
+
+            return newItem;
+        },
+        showAllItems: function () {
+            console.log(data);
+        }
+    };
 })();
 
 //IIFE module to all user interface dom manipulation
@@ -45,13 +93,20 @@ var controller = (function(budgetCon, UICon) {
     };
 
     function handleAddItem() {
-        var dataInput = UICon.getInput();
-        // 1. Get input data
+        var dataInput, newItem;
+
+        // 1. Get the field input data
+        dataInput = UICon.getInput();
         // 2. Add the item to the budget controller
+        newItem = budgetCon.addItem(
+            dataInput.type,
+            dataInput.description,
+            dataInput.value
+        );
+        budgetCon.showAllItems();
         // 3. Add the item to the UI controller
         // 4. Calculate the budget
         // 5. Display the budget on the UI
-        console.log(dataInput);
     }
 
     return {
