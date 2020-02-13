577c3dcbb1794f580b3747dbe3bd117e94370130
maxiu271299 <maksymilian.kierski@gmail.com>
2020-02-13 20:35:18 +0100

    Add initialize function to controller

--- app.js
+++ app.js
@@ -5,36 +5,60 @@ var budgetController = (function() {
 
 //IIFE module to all user interface dom manipulation
 var UIController = (function() {
+    var DOMstrings = {
+        inputType: '.add__type',
+        inputDesc: '.add__description',
+        inputVal: '.add__value',
+        button_add: '.add__btn',
+    };
+
     return {
         getInput: function() {
             return {
-                type: document.querySelector('.add__type').value,
-                description: document.querySelector('.add__description').value,
-                value: document.querySelector('.add__value').value,
+                type: document.querySelector(DOMstrings.inputType).value,
+                description: document.querySelector(DOMstrings.inputDesc).value,
+                value: document.querySelector(DOMstrings.inputVal).value,
             };
+        },
+        getDOMstrings: function() {
+            return DOMstrings;
         },
     };
 })();
 
 //IIFE module to callout budget module functions and ui module functions and connect them
 var controller = (function(budgetCon, UICon) {
+    var DOMstrings = UICon.getDOMstrings();
+
+    var setUpEventListeners = function() {
+        var addBtn = document.querySelector(DOMstrings.button_add);
+
+        //Add click event listener when someone click on add button
+        addBtn.addEventListener('click', handleAddItem);
+
+        //Add return key listener when some click it
+        document.addEventListener('keypress', function(event) {
+            if (event.keyCode === 13 || event.which === 13) {
+                handleAddItem();
+            }
+        });
+    };
+
     function handleAddItem() {
+        var dataInput = UICon.getInput();
         // 1. Get input data
         // 2. Add the item to the budget controller
         // 3. Add the item to the UI controller
         // 4. Calculate the budget
         // 5. Display the budget on the UI
-        console.log(UICon.getInput());
+        console.log(dataInput);
     }
 
-    var addBtn = document.querySelector('.add__btn');
-    //Add click event listener when someone click on add button
-    addBtn.addEventListener('click', handleAddItem);
-
-    //Add return key listener when some click it
-    document.addEventListener('keypress', function(event) {
-        if (event.keyCode === 13 || event.which === 13) {
-            handleAddItem();
-        }
-    });
+    return {
+        init: function() {
+            setUpEventListeners();
+        },
+    };
 })(budgetController, UIController);
+
+controller.init();
