f485eba9fc1a26f8470055bcce2a9cba8049a457
maxiu271299 <maksymilian.kierski@gmail.com>
2020-02-13 18:43:33 +0100

    Add handle add item function and event lisitners

--- app.js
+++ app.js
@@ -1,31 +1,35 @@
 //IIFE module to all functionality
 var budgetController = (function () {
-    var x = 10;
-    function add(y) {
-        return x + y;
-    }
-
-    return {
-        //anonymous function
-        publicAdd: function (z) {
-            return add(z);
-        }
-    }
+    //some code
 })();
 
 //IIFE module to all user interface dom manipulation
 var UIController = (function () {
-    //some come
+    //some code
 })();
-
 
 //IIFE module to connect budget module with ui module
 var connectController = (function (budgetCon, UICon) {
-    return {
-        addSomeData: function () {
-           return budgetCon.publicAdd(10);
-        }
+    function handleAddItem() {
+        // 1. Get input data
+        // 2. Add the item to the budget controller
+        // 3. Add the item to the UI controller
+        // 4. Calculate the budget
+        // 5. Display the budget on the UI
+        console.log('Adding item');
     }
+
+    var addBtn = document.querySelector('.add__btn');
+    //Add click event listener when someone click on add button
+    addBtn.addEventListener('click', handleAddItem );
+
+    //Add return key listener when some click it
+    document.addEventListener('keypress', function (event) {
+        if(event.keyCode === 13 || event.which === 13) {
+            handleAddItem();
+        }
+    });
+
 })(budgetController, UIController);
 
-console.log(connectController.addSomeData());
+
